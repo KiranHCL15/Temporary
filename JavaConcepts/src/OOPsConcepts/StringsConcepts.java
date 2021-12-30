ddf743c6e7e0422825178c265f093c124d6b8b25
kiran ghorpade <kiran ghorpade@Kiran-LaptopLenovo>
2021-12-30 10:27:31 +0530

    added print statement in stringConcepts class

--- JavaConcepts/src/OOPsConcepts/StringsConcepts.java
+++ JavaConcepts/src/OOPsConcepts/StringsConcepts.java
@@ -41,7 +41,7 @@ public class StringsConcepts {
 //  
 //        // boolean type
 //        System.out.println(true == true);
-		
+		System.out.println("String concepts");
 		
 		Thread t1 = new Thread();
         Thread t2 = new Thread();
