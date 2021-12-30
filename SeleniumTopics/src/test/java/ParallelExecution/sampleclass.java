9be5b9a9381ef5cbd7e8ce9106ac94c27273f7e9
KiranHCL15 <kiran ghorpade@Kiran-LaptopLenovo>
2021-12-30 12:27:21 +0530

    added new class

---
+++ SeleniumTopics/src/test/java/ParallelExecution/sampleclass.java
@@ -0,0 +1,12 @@
+package ParallelExecution;
+
+public class sampleclass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hi i am kiran");
+		System.out.println("helo");
+
+	}
+
+}
